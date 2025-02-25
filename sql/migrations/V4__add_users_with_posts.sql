91c020fedab47abfe9fc7b8d93e78bb7c3ad1fe7
Дмитрий Копылов <ilenisnerd@yandex.ru>
2025-02-25 23:34:03 +0300

    2

--- sql/migrations/V4__add_users_with_posts.sql
+++ sql/migrations/V4__add_users_with_posts.sql
@@ -1,7 +1,7 @@
 WITH new_users AS (
     INSERT INTO users (name)
     VALUES ('Alex'), ('Max')
-    RETURNING id, user
+    RETURNING id, name
 )
 -- Добавляем по одному посту для каждого нового пользователя
 INSERT INTO posts (text, owner_id)
